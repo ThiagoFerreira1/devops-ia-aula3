43018bf4ced316de4e537b0a252bb0e8151600a3
Thiago Ferreira <thiagoalvesferreira.taf@gmail.com>
2026-06-10 23:17:43 -0300

    Ajusta funções de baixa

--- src_baixa/app.py
+++ src_baixa/app.py
@@ -1,12 +1,7 @@
-def verificar_numero(numero):
-    if numero > 0:
-        return "Número positivo"
-    elif numero < 0:
-        return "Número negativo"
-    else:
-        return "Número zero"
+def somar(a, b):
+    return a + b
 
-def eh_par(numero):
-    if numero % 2 == 0:
-        return True
-    return False
+def dividir (a, b):
+    if b == 0:
+        raise ValueError("Divisão por zero não é permitida.")
+    return a / b
